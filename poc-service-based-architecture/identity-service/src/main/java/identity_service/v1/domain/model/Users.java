05fa56bb42b5cfc48f8716ec87bd2b144a9d96be
felipematheus1337 <lipehbr@gmail.com>
2026-03-05 16:05:59 -0300

    feature: sba -> identity-service, created user entity.

---
+++ poc-service-based-architecture/identity-service/src/main/java/identity_service/v1/domain/model/Users.java
@@ -0,0 +1,28 @@
+package identity_service.v1.domain.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(name = "email", unique = true)
+    private String email;
+
+    private LocalDateTime createdAt;
+
+}
